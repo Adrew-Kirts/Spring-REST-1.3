95046b95fb08a22c6f4a9da761f6c9624931eeb8
Ezra Strikwerda <ezra@strikwerda.fr>
2023-09-01 13:44:31 +0200

    Added Eureka client

--- src/main/java/fr/campusnumerique/restservice/ResponseApplication.java
+++ src/main/java/fr/campusnumerique/restservice/ResponseApplication.java
@@ -3,11 +3,16 @@ package fr.campusnumerique.restservice;
 // Importing classes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // Annotation
 @SpringBootApplication
 @EnableSwagger2
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 // Main class
 public class ResponseApplication {
 
